dbf86b5e2992b10abbfd99982b5f367cb4432d51
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-04-03 21:21:06 +0530

    Assets

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabLayout = () => {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="subsciptions" options={{ title: "Subsciptions" }} />
+      <Tabs.Screen name="insights" options={{ title: "Insights" }} />
+      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
