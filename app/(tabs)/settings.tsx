7c5491b3c73ad6bb105f197142db2f7840fbb1f7
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-04-04 12:37:53 +0530

    Constants

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text } from "react-native";
+import React from "react";
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Settings = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Settings</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default Settings
+export default Settings;
